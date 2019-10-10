fe333ca24f3c9b491b66886d7031163f47eddf70
ipipman <61867170@qq.com>
2019-10-10 17:28:40 +0800

    fix

---
+++ test/testKoa2.js
@@ -0,0 +1,54 @@
+const myKoa2 = require('../src/myKoa2');
+
+const app = new myKoa2();
+app.listen(2000);
+
+/***
+ * 测试MyKoa2框架Next中间件原型链调用（圆葱模型）
+ * 测试结果：{第一次，第二次，第三次，第四次}
+ * {
+        第一次 start
+        第二次 start
+        第三次 start
+        第四次 start
+        sleep 1s
+        第四次 end
+        第三次 end
+        第二次 end
+        第一次 end
+        耗时：1s
+ * }
+ *
+ */
+app.use(async function (ctx, next) {
+    const startTs = new Date().getSeconds();
+    console.log('第一次', 'start');
+
+    await next();
+
+    console.log('第一次', 'end');
+    const endTs = new Date().getSeconds();
+
+    console.log(`耗时：${endTs - startTs}s`);
+    ctx.response.end();
+});
+
+app.use(function (ctx, next) {
+    console.log('第二次', 'start');
+    next();
+    console.log('第二次', 'end');
+});
+
+app.use(async function (ctx, next) {
+    console.log('第三次', 'start');
+    next();
+    console.log('第三次', 'end');
+});
+
+app.use(async function (ctx, next) {
+    console.log('第四次', 'start');
+    console.log('sleep 1s');
+    for (var start = +new Date; +new Date - start <= 1000;) {  // sleep 1s
+    }
+    console.log('第四次', 'end');
+});
